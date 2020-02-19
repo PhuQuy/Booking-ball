c5dcfe2b04e318460cacec8f79b8479a2ea58d73
PhuQuy <phuquy.tool@gmail.com>
2020-02-19 10:09:55 +0700

    update

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from '../images/logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Home = lazy(() => import("./home"));
+const ContactUs = lazy(() => import("./contactUs"));
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/contact-us" component={ContactUs} />
+      </Switch>
+    </Suspense>
+  </Router>
+);
 
 export default App;
