b6bfa203a9a41c7b340ab91f843383830cb42bd7
KhanhNguyen <nvkhanh1775@gmail.com>
2020-02-19 11:10:29 +0700

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,19 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { Link } from "react-router-dom";
+import './NavBar.scss'
 
 const NavBar = () => {
     return(
         <div>
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="h6" noWrap>
-                    Menu 1
+                <Typography variant="h6" noWrap className="nav-item">
+                    <Link to="/">Home</Link>
                 </Typography>
-                <Typography variant="h6" noWrap>
-                    Menu 2
+                <Typography variant="h6" noWrap className="nav-item">
+                    <Link to="/contact-us">Contact Us</Link>
                 </Typography>
             </Toolbar>
         </AppBar>
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,14 @@
 import React, { Suspense, lazy } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 
 const Home = lazy(() => import("./home"));
 const ContactUs = lazy(() => import("./contactUs"));
 
 const App = () => (
   <Router>
+  <NavBar />
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
         <Route exact path="/" component={Home} />
