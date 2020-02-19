1751e35617d2e9740d5c8b92f4197a01bb8fd508
KhanhNguyen <nvkhanh1775@gmail.com>
2020-02-19 10:15:33 +0700

    navbar

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from '../images/logo.svg';
 import './App.css';
+import NavBar from '../components/NavBar'
 
 function App() {
   return (
+
     <div className="App">
+      <div>
+        <NavBar />
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
