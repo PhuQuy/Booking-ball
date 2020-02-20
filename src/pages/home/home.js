d417b9532072db9a3c2fc5b60e34b4001928f697
PhuQuy <phuquy.tool@gmail.com>
2020-02-20 15:28:56 +0700

    update

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,12 +2,15 @@ import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import React, { Component } from "react";
 import "./home.scss";
+import Search from "../../components/search/search";
 
 export default class home extends Component {
   render() {
     return (
       <React.Fragment>
         <CssBaseline />
+        <Search />
+
         <Container maxWidth="lg">
           {/* <CenteredGrid />
           <SelectForm/>
